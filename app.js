6b2334b7fdee11bd14033aab80b62a5c0be82c51
abidalili <abidalili1995@gmail.com>
2023-06-02 17:35:16 +0300

    code updated

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ const menu = [
 // get parent element
 const sectionCenter = document.querySelector(".section-center");
 const btnContainer = document.querySelector(".btn-container");
+
 // display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
   diplayMenuItems(menu);
